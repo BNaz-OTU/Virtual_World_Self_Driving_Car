85477bf145d5b08435fec5652c080aafd1105b99
BNaz-OTU <bilal.nazar@ontariotechu.net>
2025-01-16 14:48:40 -0500

    Added roofs to the buildings and fixed minor details with the scaling of items

--- js/math/utils.js
+++ js/math/utils.js
@@ -54,30 +54,6 @@ function angle(p) {
   return Math.atan2(p.y, p.x);
 }
 
-// function getIntersection(A, B, C, D) {
-//   const tTop = (D.x - C.x) * (A.y - C.y) - (D.y - C.y) * (A.x - C.x);
-//   const uTop = (C.y - A.y) * (A.x - B.x) - (C.x - A.x) * (A.y - B.y);
-//   const bottom = (D.y - C.y) * (B.x - A.x) - (D.x - C.x) * (B.y - A.y);
-
-//   if (bottom != 0) {
-//     const t = tTop / bottom;
-//     const u = uTop / bottom;
-//     if (t >= 0 && t <= 1 && u >= 0 && u <= 1) {
-//       return {
-//         x: lerp(A.x, B.x, t),
-//         y: lerp(A.y, B.y, t),
-//         offset: t,
-//       };
-//     }
-//   }
-
-//   return null;
-// }
-
-// function lerp(a, b, t) {
-//   return a + (b - a) * t;
-// }
-
 function getIntersection(A, B, C, D) {
   const tTop = (D.x - C.x) * (A.y - C.y) - (D.y - C.y) * (A.x - C.x);
   const uTop = (C.y - A.y) * (A.x - B.x) - (C.x - A.x) * (A.y - B.y);
@@ -110,4 +86,11 @@ function lerp2D(A, B, t) {
 function getRandomColor() {
   const hue = 290 + Math.random() * 260;
   return "hsl(" + hue + ", 100%, 60%)";
+}
+
+function getFake3dPoint(point, viewPoint, height) {
+  const dir = normalize(subtract(point, viewPoint));
+  const dist = distance(point, viewPoint);
+  const scaler = Math.atan(dist / 300) / (Math.PI / 2);
+  return add(point, scale(dir, height * scaler));
 }
